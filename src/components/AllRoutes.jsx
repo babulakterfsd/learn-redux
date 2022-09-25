1663fc800bd32504d30254de6888b17c2b41dbd0
Babul Akter <babulakterfsd@gmail.com>
2022-09-25 09:55:11 +0600

    files and folders have been customized

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -9,24 +9,6 @@ function AllRoutes() {
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Navigate to="/" replace />} />
             <Route path="*" element={<NotFound />} />
-            {/* <Route path="/packages/:packageid" element={<PrivateOutlet />}>
-                <Route path="" element={<Checkout />} />
-            </Route>
-            <Route path="/dashboard" element={<PrivateOutlet />}>
-                <Route path="" element={<Dashboard />}>
-                    <Route path="summary" element={<Summary />} />
-                    <Route path="allusers" element={<Users />}>
-                        <Route path="singleuserdetails" element={<SingleUserDetails />} />
-                    </Route>
-                    <Route path="allpackages" element={<AllPackages />} />
-                    <Route path="addnewpackage" element={<AddNewpackage />} />
-                    <Route path="allorders" element={<AllOrders />} />
-                    <Route path="myorders" element={<MyOrders />} />
-                    <Route path="addreview" element={<AddReview />} />
-                    <Route path="profile" element={<Profile />} />
-                    <Route path="*" element={<NotFoundInDashboard />} />
-                </Route>
-            </Route> */}
         </Routes>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
     return (
         <div>
-            <h1 className="text-darkChocolate">hello</h1>
+            <h1 className="text-darkChocolate text-center text-3xl capitalize">hello</h1>
         </div>
     );
 }
