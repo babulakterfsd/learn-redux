8646dfaa02adb8e91cd9f01ebf78a4dbc1deddb2
Babul Akter <babulakterfsd@gmail.com>
2022-10-10 10:13:59 +0600

    project initialized

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import TodoList from '../components/TodoList';
 
 function Home() {
     return (
-        <div className="h-screen flex justify-center items-center bg-slate-400 flex-col lg:flex-row">
-            <p>home</p>
+        <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+            <Navbar />
+
+            <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+                <Header />
+
+                <hr className="mt-4" />
+
+                <TodoList />
+
+                <hr className="mt-4" />
+
+                <Footer />
+            </div>
         </div>
     );
 }
