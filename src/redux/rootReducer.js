6926c8436654dd2d9d9b915237d46e34f416f4d5
Babul Akter <babulakterfsd@gmail.com>
2022-10-18 20:49:08 +0600

    project initialized

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,7 @@
 import { combineReducers } from 'redux';
-import filterReducer from './filters/filterReducer';
-import todoReducer from './todos/todoReducer';
 
 const rootReducer = combineReducers({
-    todos: todoReducer,
-    filters: filterReducer,
+    //
 });
 
 export default rootReducer;
