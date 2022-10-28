86a049db52ec5c6e477b8f83bf923c9332019037
Babul Akter <babulakterfsd@gmail.com>
2022-10-28 12:21:20 +0600

    tags slice added

--- src/components/tags/Tag.jsx
+++ src/components/tags/Tag.jsx
@@ -1,5 +1,5 @@
-export default function Tag() {
+export default function Tag({ tag }) {
     return (
-        <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">react</div>
+        <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">{tag}</div>
     );
 }
--- src/rtk/store/store.js
+++ src/rtk/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import tagsReducer from '../features/tags/tagsSlice';
 import videosReducer from '../features/videos/videosSlice';
 import myMiddleware from '../middlewares/myMiddleware';
 
 const store = configureStore({
     reducer: {
         videos: videosReducer,
+        tags: tagsReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(myMiddleware),
 });
