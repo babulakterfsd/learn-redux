424671e913a3915e2b092b90c2d17cbc04a30abd
Babul Akter <babulakterfsd@gmail.com>
2022-11-01 13:04:57 +0600

    pagination done

--- src/components/grid/VideoGrid.jsx
+++ src/components/grid/VideoGrid.jsx
@@ -24,7 +24,7 @@ export default function VideGrid() {
         content = videos?.map((video) => <VideoGridItem key={video.id} video={video} />);
 
     return (
-        <section className="pt-12">
+        <section className="pt-16">
             <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-screen">
                 {content}
             </div>
