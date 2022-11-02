94d7dd23820160a8f31c1ad1e3b7bb6dbf21e9b0
Babul Akter <babulakterfsd@gmail.com>
2022-11-02 23:50:41 +0600

    project initialized

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -2,22 +2,15 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from '../views/Home';
 import NotFound from '../views/NotFound';
-import Video from '../views/Video';
-import Footer from './Footer';
-import Navbar from './navbar/Navbar';
 
 function AllRoutes() {
     return (
-        <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/home" element={<Navigate to="/" replace />} />
-                <Route path="/videos/:videoId" element={<Video />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-            <Footer />
-        </>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+
+            <Route path="*" element={<NotFound />} />
+        </Routes>
     );
 }
 
