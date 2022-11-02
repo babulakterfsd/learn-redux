70740bf241ee6f9d9cfa6f00fd25814e49a87acc
Babul Akter <babulakterfsd@gmail.com>
2022-11-02 23:43:02 +0600

    test

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,7 @@
-import VideoGrid from '../components/grid/VideoGrid';
-import Pagination from '../components/Pagination';
-import Tags from '../components/tags/Tags';
-
 export default function Home() {
     return (
-        <>
-            <Tags />
-            <VideoGrid />
-            <Pagination />
-        </>
+        <div>
+            <p>This is home</p>
+        </div>
     );
 }
