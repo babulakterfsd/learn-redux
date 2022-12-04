1086ca83b396e5939dab12a05fd237a3682e49f5
Babul Akter <babulakterfsd@gmail.com>
2022-12-04 13:52:22 +0600

    toastify added

--- src/components/edit/Form.jsx
+++ src/components/edit/Form.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable no-alert */
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useEditVideoMutation } from '../../rtk/features/api/apiSlice';
 import Error from '../ui/Error';
-import Success from '../ui/Success';
 import TextArea from '../ui/TextArea';
 import TextInput from '../ui/TextInput';
 
@@ -45,100 +48,122 @@ export default function Form({ video }) {
         editVideo({ id, data });
     };
 
+    const navigate = useNavigate();
+
+    if (isSuccess) {
+        toast.success('Video updated successfully!', {
+            toastId: 'success1',
+            position: 'top-right',
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+        });
+        setTimeout(() => {
+            navigate('/');
+        }, 2000);
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="shadow overflow-hidden sm:rounded-md">
-                <div className="px-4 py-5 bg-white sm:p-6">
-                    <div className="grid grid-cols-6 gap-6">
-                        <div className="col-span-6 sm:col-span-3">
-                            <TextInput
-                                title="Video Title"
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                                required
-                            />
-                        </div>
+        <>
+            <form onSubmit={handleSubmit}>
+                <div className="shadow overflow-hidden sm:rounded-md">
+                    <div className="px-4 py-5 bg-white sm:p-6">
+                        <div className="grid grid-cols-6 gap-6">
+                            <div className="col-span-6 sm:col-span-3">
+                                <TextInput
+                                    title="Video Title"
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    required
+                                />
+                            </div>
 
-                        <div className="col-span-6 sm:col-span-3">
-                            <TextInput
-                                title="Author"
-                                value={author}
-                                onChange={(e) => setAuthor(e.target.value)}
-                                required
-                            />
-                        </div>
+                            <div className="col-span-6 sm:col-span-3">
+                                <TextInput
+                                    title="Author"
+                                    value={author}
+                                    onChange={(e) => setAuthor(e.target.value)}
+                                    required
+                                />
+                            </div>
 
-                        <div className="col-span-6">
-                            <TextArea
-                                title="Description"
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                                required
-                            />
-                        </div>
+                            <div className="col-span-6">
+                                <TextArea
+                                    title="Description"
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    required
+                                />
+                            </div>
 
-                        <div className="col-span-6">
-                            <TextInput
-                                title="YouTube Video link"
-                                value={link}
-                                onChange={(e) => setLink(e.target.value)}
-                                required
-                            />
-                        </div>
+                            <div className="col-span-6">
+                                <TextInput
+                                    title="YouTube Video link"
+                                    value={link}
+                                    onChange={(e) => setLink(e.target.value)}
+                                    required
+                                />
+                            </div>
 
-                        <div className="col-span-6">
-                            <TextInput
-                                title="Thumbnail link"
-                                value={thumbnail}
-                                onChange={(e) => setThumbnail(e.target.value)}
-                                required
-                            />
-                        </div>
+                            <div className="col-span-6">
+                                <TextInput
+                                    title="Thumbnail link"
+                                    value={thumbnail}
+                                    onChange={(e) => setThumbnail(e.target.value)}
+                                    required
+                                />
+                            </div>
 
-                        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                            <TextInput
-                                title="Upload Date"
-                                value={date}
-                                onChange={(e) => setDate(e.target.value)}
-                                required
-                            />
-                        </div>
+                            <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                                <TextInput
+                                    title="Upload Date"
+                                    value={date}
+                                    onChange={(e) => setDate(e.target.value)}
+                                    required
+                                />
+                            </div>
 
-                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <TextInput
-                                title="Video Duration"
-                                value={duration}
-                                onChange={(e) => setDuration(e.target.value)}
-                                required
-                            />
-                        </div>
+                            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                                <TextInput
+                                    title="Video Duration"
+                                    value={duration}
+                                    onChange={(e) => setDuration(e.target.value)}
+                                    required
+                                />
+                            </div>
 
-                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <TextInput
-                                title="Video no of views"
-                                value={views}
-                                onChange={(e) => setViews(e.target.value)}
-                                required
-                            />
+                            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                                <TextInput
+                                    title="Video no of views"
+                                    value={views}
+                                    onChange={(e) => setViews(e.target.value)}
+                                    required
+                                />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button
-                        disabled={isLoading}
-                        type="submit"
-                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-indigo-500"
-                    >
-                        Save
-                    </button>
-                    {!isLoading && !isError && isSuccess && (
+                    <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                        <button
+                            disabled={isLoading}
+                            type="submit"
+                            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-indigo-500"
+                        >
+                            Save
+                        </button>
+                        {/* {!isLoading && !isError && isSuccess && (
                         <Success message="Video updated successfully" />
-                    )}
-                    {!isLoading && isError && (
-                        <Error message="An error occured while adding video" />
-                    )}
+                    )} */}
+                        {!isLoading && isError && (
+                            <Error message="An error occured while adding video" />
+                        )}
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+            <ToastContainer />
+        </>
     );
 }
